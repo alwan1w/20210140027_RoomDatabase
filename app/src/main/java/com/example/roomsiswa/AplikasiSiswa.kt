150c4a06c970cdf7965e9df2d21c89b4197b3caa
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 17:43:09 +0700

    menambah isi di AplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repsotori.ContainerApp
+import com.example.roomsiswa.repsotori.ContainerDataApp
+
+class AplikasiSiswa : Application(){
+    lateinit var container : ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
