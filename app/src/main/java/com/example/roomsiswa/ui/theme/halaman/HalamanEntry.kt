3809d0b0f4698ef7e8b402211a4bd37176749bc2
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 18:11:57 +0700

    membuat HalamanEntry

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,2 +1,20 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
