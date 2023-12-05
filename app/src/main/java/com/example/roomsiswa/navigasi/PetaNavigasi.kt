64e1b1040ca7639f381f232152bd85df3dfa9fa8
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 19:00:57 +0700

    menambah HostNavigasi

--- app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
@@ -12,8 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
+import com.example.roomsiswa.ui.theme.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.theme.halaman.DestinasiHome
+import com.example.roomsiswa.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.theme.halaman.HomeScreen
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController()) {
@@ -45,7 +51,19 @@ fun SiswaTopAppBar(
         }
     )
 }
+@Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-){}
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route,modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
+}
