e4e4602de78c669afd0440dcef1e4611ef697e60
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 18:20:24 +0700

    Menambahkan Function SiswaApp dan HostNavigasi

--- app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
@@ -1,2 +1,16 @@
 package com.example.roomsiswa.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()) {
+    HostNavigasi(navController = navController)
+
+}
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){}
