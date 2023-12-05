7e4e17ec5c09c52918e11e7c3433e58bf0d609e8
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 16:59:10 +0700

    Membuat class EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repsotori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+
+}
