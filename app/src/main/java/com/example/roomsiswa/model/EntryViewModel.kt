4795d8883532fc25b51ecfc79ddb23c7a425cc22
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 17:06:35 +0700

    membuat variable, data class uistatesiswa dan detailsiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,9 +1,22 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repsotori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
-
-
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    data class UIStateSiswa(
+        val detailSiswa: DetailSiswa = DetailSiswa(),
+        val isEntryValid: Boolean = false
+    )
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telpon: String = "",
+    )
 }
