bcbb5cf4215957fa0970e78aa99fd469cf8866f9
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2023-12-05 17:23:51 +0700

    Membuat fun siswa toDetailSiswa dan toUiStateSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -39,5 +39,16 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
         alamat = alamat,
         telpon = telpon
     )
+    fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+    fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon=telpon
+    )
 
 }
